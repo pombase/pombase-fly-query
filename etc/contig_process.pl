6bd6aa84a64d36e31536faebadb374a9c2228677
Kim Rutherford <kmr@aska.gen.nz>
2026-08-15 02:44:46 +1200

    Add script for processing contigs
    Refs pombase/pombase-fly-query#1

---
+++ etc/contig_process.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+
+my $current = undef;
+my $seen_qual = 0;
+my $seen_ac = 0;
+
+while (<>) {
+  if (/^(ID|AC|FT|FH|SQ|\/\/|  )/) {
+    if ($1 eq 'AC') {
+      if ($seen_ac) {
+        next;
+      } else {
+        $seen_ac = 1;
+        s/;.*/;/;
+      }
+    }
+    if (/^FT/) {
+      if (/^FT   (\S+)/) {
+        $current = $1;
+        $seen_qual = 0;
+      } else {
+        if (m|FT                   /|) {
+          $seen_qual = 1;
+        }
+      }
+
+      if ($current eq 'CDS') {
+        if (!$seen_qual) {
+          print;
+        } else {
+          if (/^(FT\s+\/)(gene|gene_synonym|locus_tag|product|protein_id)=(.*)/) {
+            my $q_name = $2;
+            if ($q_name eq 'gene') {
+              $q_name = 'primary_name';
+            } else {
+              if ($q_name eq 'gene_synonym') {
+                $q_name = 'synonym';
+              } else {
+                if ($q_name eq 'locus_tag') {
+                  $q_name = 'systematic_id';
+                }
+              }
+            }
+            my $q_value = $3;
+            if ($q_value !~ /\\/) {
+              print "$1$q_name=$q_value\n";
+            }
+          }
+        }
+      }
+    } else{
+      print;
+    }
+  }
+}
