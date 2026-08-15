256b1f09ce62e09d683968ff25c909f35c8616bb
Kim Rutherford <kmr@aska.gen.nz>
2026-08-16 03:51:30 +1200

    Fix copy of GO-CAM YAML to container

--- load-all.sh
+++ load-all.sh
@@ -1111,7 +1111,7 @@ find $CURRENT_BUILD_DIR/fasta -name '*.fa' | xargs gzip -1f
 
 cp $LOG_DIR/$log_file.web-json-write $CURRENT_BUILD_DIR/logs/
 
-cp -r $GIT_DIR/pombase-fly-query/go-cams $CURRENT_BUILD_DIR/web-json/go-cam
+cp -r $GIT_DIR/go-cams $CURRENT_BUILD_DIR/web-json/go-cam
 
 DB_BASE_NAME=`echo $DB | sed 's/-v[0-9]$//'`
 
